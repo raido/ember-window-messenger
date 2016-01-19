9533498a22f0f616a45e14874d564c13fd93cfbd
Raido Kuli <raido357@gmail.com>
2016-01-19 05:29:21 +0200

    chore(deps): Upgrade toolset

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import defaultResolver from 'ember/resolver';
+import Resolver from 'ember-resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
@@ -10,7 +10,7 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: defaultResolver
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
