6ca26f43acfc453920e80e275c01bafddb7bce40
Raido Kuli <raido357@gmail.com>
2016-03-30 13:01:47 -0700

    tests: Fix missing coverage flag from testem.js

--- testem.js
+++ testem.js
@@ -1,7 +1,8 @@
 /*jshint node:true*/
+// jscs: disable
 module.exports = {
   "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
+  "test_page": "tests/index.html?hidepassed&coverage",
   "disable_watching": true,
   "launch_in_ci": [
     "PhantomJS"
