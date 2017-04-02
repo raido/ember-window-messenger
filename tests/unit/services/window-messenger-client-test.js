c0c760e459d696ce250c59555066ac75044111ea
Raido Kuli <raido357@gmail.com>
2017-04-02 10:53:25 +0300

    Add some more sensible tests for client and server

--- tests/unit/services/window-messenger-client-test.js
+++ tests/unit/services/window-messenger-client-test.js
@@ -1,11 +1,45 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
-moduleFor('service:window-messenger-client', 'Unit | Service | Client', {
+const { getOwner } = Ember;
+
+moduleFor('service:window-messenger-client', 'Unit | Service | window messenger client', {
   // Specify the other units that are required for this test.
-  needs: ['service:window-messenger-events']
+  needs: ['service:window-messenger-events', 'service:window-messenger-server']
 });
 
-test('it works', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+test('it should receive response from the server', function(assert) {
+  let client = this.subject();
+  let server = getOwner(client).lookup('service:window-messenger-server');
+
+  server.on('client-request', (resolve) => {
+    resolve('Hello');
+  });
+  client.fetch('client-request').then((response) => assert.equal(response, 'Hello'));
+});
+
+test('it should receive rejection from the server', function(assert) {
+  let client = this.subject();
+  let server = getOwner(client).lookup('service:window-messenger-server');
+
+  server.on('client-request', (resolve, reject) => {
+    reject('Failed');
+  });
+  client.fetch('client-request').catch((response) => assert.equal(response, 'Failed'));
+});
+
+test('it should receive object from the server', function(assert) {
+  let client = this.subject();
+  let server = getOwner(client).lookup('service:window-messenger-server');
+
+  let model = {
+    complex: {
+      id: 1
+    }
+  };
+
+  server.on('client-request', (resolve, reject) => {
+    resolve(model);
+  });
+  client.fetch('client-request').then((response) => assert.deepEqual(response, model));
 });
--- tests/unit/services/window-messenger-server-test.js
+++ tests/unit/services/window-messenger-server-test.js
@@ -1,13 +1,44 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
-moduleFor('service:window-messenger-server', 'Unit | Service | server', {
+const { getOwner } = Ember;
+
+moduleFor('service:window-messenger-server', 'Unit | Service | window messenger server', {
   // Specify the other units that are required for this test.
-  needs: ['service:window-messenger-events']
+  needs: ['service:window-messenger-events', 'service:window-messenger-client']
 });
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let service = this.subject();
+test('it should receive client\'s request', function(assert) {
+  let server = this.subject();
+  let client = getOwner(server).lookup('service:window-messenger-client');
 
-  assert.ok(service);
+  server.on('client-request', (/*resolve, reject, query*/) => {
+    assert.ok(true);
+  });
+  client.fetch('client-request');
+});
+
+test('it should not receive client\'s request if not a match', function(assert) {
+  assert.expect(0);
+
+  let server = this.subject();
+  let client = getOwner(server).lookup('service:window-messenger-client');
+
+  server.on('client-request', (/*resolve, reject, query*/) => {
+    assert.ok(true);
+  });
+  client.fetch('client-request-no-match');
+});
+
+test('it should receive query from client', function(assert) {
+  let server = this.subject();
+  let client = getOwner(server).lookup('service:window-messenger-client');
+
+  server.on('client-request', (resolve, reject, query) => {
+    assert.equal(query.id, 1, 'it should have got query parameters');
+  });
+
+  client.fetch('client-request', {
+    id: 1
+  });
 });
