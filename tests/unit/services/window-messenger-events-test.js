d960bb7cba602edfb5febd37d46f5eb2950db6a2
Raido Kuli <raido357@gmail.com>
2017-04-02 18:14:31 +0300

    Add test case for not allowed origin

--- tests/unit/services/window-messenger-events-test.js
+++ tests/unit/services/window-messenger-events-test.js
@@ -32,3 +32,21 @@ test('it should register and deregister message event listener', function(assert
   // send second message
   window.postMessage(message, '*');
 });
+
+test('it should handle message, if not allowed origin', function(assert) {
+  assert.expect(0);
+
+  let service = this.subject();
+  let message = JSON.stringify({
+    id: +new Date(),
+    type: 'test-dummy',
+    name: 'hello-world',
+    query: {}
+  });
+
+  service.on('from:test-dummy', () => {
+    assert.ok(true);
+  });
+
+  window.postMessage(message, 'http://localhost:9999');
+})
